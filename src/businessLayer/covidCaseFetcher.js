d7ca67502ad4ff05fe1ccb517b0f475e8547b70a
Gokul Kulkarni <gokul.cs5@gmail.com>
2020-12-02 00:30:12 +0530

    invalid country code handling

--- src/businessLayer/covidCaseFetcher.js
+++ src/businessLayer/covidCaseFetcher.js
@@ -1,33 +1,56 @@
-const { getCountryCasesDL, getAllCovidDetailsDL } = require('../dao/covidCaseDAO');
-const { sendMessage } = require('../twilio/config');
+const {
+  getCountryCasesDL,
+  getAllCovidDetailsDL,
+} = require("../dao/covidCaseDAO");
+const { sendMessage } = require("../twilio/config");
 
 const getCountryCasesBL = async (countryCode, from) => {
   const covid = await getCountryCasesDL(countryCode);
-  const message = countryCode + ' Active Cases ' + covid.cases;
-  sendMessage(message, from);
-}
+  if (covid) {
+    const message = countryCode + " Active Cases " + covid.cases;
+    sendMessage(message, from);
+  } else {
+    const message = countryCode + process.env.INVALID_COUNTRY_CODE;
+    sendMessage(message, from);
+  }
+};
 
 const getCountryDeathsBL = async (countryCode, from) => {
   const covid = await getCountryCasesDL(countryCode);
-  const message = countryCode + ' Deaths ' + covid.deaths;
-  sendMessage(message, from);
-}
+  if (covid) {
+    const message = countryCode + " Deaths " + covid.deaths;
+    sendMessage(message, from);
+  } else {
+    const message = countryCode + process.env.INVALID_COUNTRY_CODE;
+    sendMessage(message, from);
+  }
+};
 
 const getTotalCasesBL = async (from) => {
   const covid = await getAllCovidDetailsDL();
-  const message = `Total Active Cases ${covid.cases}`
-  sendMessage(message, from);
-}
+  if (covid) {
+    const message = `Total Active Cases ${covid.cases}`;
+    sendMessage(message, from);
+  } else {
+    const message = process.env.UNABLE_TO_GET_DATA;
+    sendMessage(message, from);
+  }
+};
 
 const getTotalDeathsBL = async (from) => {
   const covid = await getAllCovidDetailsDL();
-  const message = `Total Deaths ${covid.deaths}`
-  sendMessage(message, from);
-}
+  if (covid) {
+    const message = `Total Deaths ${covid.deaths}`;
+    sendMessage(message, from);
+  } else {
+    const message = process.env.UNABLE_TO_GET_DATA;
+    sendMessage(message, from);
+  }
+};
 
 module.exports = {
   getCountryCasesBL,
   getCountryDeathsBL,
   getTotalCasesBL,
-  getTotalDeathsBL
-}
+  getTotalDeathsBL,
+};
